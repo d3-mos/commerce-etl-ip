b4ebff55837ef0b5f2c2b7a71da9604d545aa9d8
Ricardo Bermúdez Bermúdez <ricardob.sistemas@gmail.com>
2019-11-05 17:54:42 -0600

    Add file tools

---
+++ src/main/java/com/globalhitss/claropay/cercademi/commerceetlip/util/FileTools.java
@@ -0,0 +1,73 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+
+/**
+ * https://www.baeldung.com/java-compress-and-uncompress
+ */
+public class FileTools
+{
+  
+  /** */
+  public void download(String fileURI, String fileName)
+    throws IOException
+  { 
+    BufferedInputStream in = new BufferedInputStream(new URL(fileURI).openStream();
+    FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+    byte dataBuffer[] = new byte[1024];
+    int bytesRead;
+    
+    while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+      fileOutputStream.write(dataBuffer, 0, bytesRead);
+    }
+  }
+
+  /** */
+  public void unzip(final String fileZip, final String path)
+    throws IOException 
+  {
+    final File destDir = new File(path);
+    final byte[] buffer = new byte[1024];
+    final ZipInputStream zis = new ZipInputStream(
+      new FileInputStream(fileZip)
+    );
+    
+    ZipEntry zipEntry = zis.getNextEntry();
+    while (zipEntry != null) {
+      final File newFile = newFile(destDir, zipEntry);
+      new File(newFile.getParent()).mkdirs(); // [1]
+      final FileOutputStream fos = new FileOutputStream(newFile);
+      
+      int len;
+      while ((len = zis.read(buffer)) > 0) {
+          fos.write(buffer, 0, len);
+      }
+      fos.close();
+      zipEntry = zis.getNextEntry();
+    }
+    zis.closeEntry();
+    zis.close();
+  }
+    
+  /**
+   * @see https://snyk.io/research/zip-slip-vulnerability
+   */
+  public File newFile(File destinationDir, ZipEntry zipEntry)
+    throws IOException
+  {
+    File destFile = new File(destinationDir, zipEntry.getName());
+    String destDirPath  = destinationDir.getCanonicalPath();
+    String destFilePath = destFile.getCanonicalPath();
+
+    if (!destFilePath.startsWith(destDirPath + File.separator)) {
+      throw new IOException("File outside of the target dir.");
+    }
+    return destFile;
+  }
+}
