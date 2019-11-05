9df3f9ae9a6b07f3d797cd440c854a5dfab04da7
Ricardo Bermúdez Bermúdez <ricardob.sistemas@gmail.com>
2019-11-05 15:35:43 -0600

    Add skeleton of ETL.

---
+++ src/main/java/com/globalhitss/claropay/cercademi/commerceetlip/etl/ETLIp2Location.java
@@ -0,0 +1,12 @@
+package com.globalhitss.claropay.cercademi.commerceetlip.etl;
+
+public class ETLIp2Location
+{
+    public void extract(){}
+
+    public void transform(){}
+
+    public void load(){}
+
+    public void run(){}
+}
