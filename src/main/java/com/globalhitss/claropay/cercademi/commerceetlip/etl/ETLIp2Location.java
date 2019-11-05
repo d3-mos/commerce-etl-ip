c1d89b806c9c5d7fa6ff544ad5f21c4d16d53f09
Ricardo Bermúdez Bermúdez <ricardob.sistemas@gmail.com>
2019-11-05 16:23:42 -0600

    End ETL

--- src/main/java/com/globalhitss/claropay/cercademi/commerceetlip/etl/ETLIp2Location.java
+++ src/main/java/com/globalhitss/claropay/cercademi/commerceetlip/etl/ETLIp2Location.java
@@ -1,12 +1,72 @@
 package com.globalhitss.claropay.cercademi.commerceetlip.etl;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import static java.lang.Double.parseDouble;
+import static java.lang.Long.parseLong;
+
+import com.globalhitss.claropay.cercademi.commerceetlip.appservice.AppProperties;
+import com.globalhitss.claropay.cercademi.commerceetlip.dao.IPLocationDao;
+import com.globalhitss.claropay.cercademi.commerceetlip.model.IPLocation;
+
 public class ETLIp2Location
 {
-    public void extract(){}
+  public List<String> extract(String path)
+    throws IOException
+  {
+    BufferedReader csvFile = new BufferedReader( new FileReader(path) );
+    List<String> lines = csvFile
+      .lines()
+      .skip(1)
+      .filter(line -> line.matches(".*\"MX\".*"))
+      .map(line -> line.replaceAll("\"", ""))
+      .collect(Collectors.toList());
+    csvFile.close();
+    return lines;
+  }
 
-    public void transform(){}
 
-    public void load(){}
+  public List<IPLocation> transform(List<String> rows)
+  {
+    return rows.stream().map( row -> {
+      try {
+        String[] rowFields = row.split(",");
 
-    public void run(){}
+        return new IPLocation(
+          parseLong(rowFields[0]),
+          parseLong(rowFields[1]),
+          parseDouble(rowFields[6]),
+          parseDouble(rowFields[7]),
+          rowFields[8],
+          "ip2location"
+        );
+      } catch(Exception e){}
+
+      return null;
+    })
+    .filter(location -> location!=null)
+    .collect(Collectors.toList());
+  }
+
+  public void load(List<IPLocation> locations)
+  {
+    new IPLocationDao().insert(locations);
+  }
+
+  public void run()
+  {
+    try{
+      System.out.println("Inicia ETL");
+      List<String> rows = extract(AppProperties.get("file.ip2location_database"));
+      System.out.println(" - Termina extracción: " + rows.size());
+      List<IPLocation> objs = transform(rows);
+      System.out.println(" - Termina transformación: " + objs.size());
+      load(objs);
+      System.out.println(" - Termina carga");
+      
+    }catch(Exception e){}
+  }
 }
